05c7fe4dbe653d87c147f6fd3a37111d6586f2c9
Kevin_was_here <xuyang4@ualberta.ca>
2022-07-18 14:17:47 -0600

    sidebar+layout checked for faults

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,13 +1,23 @@
 
+import { Outlet } from 'react-router-dom';
 import LeftSidebar from '../LeftSidebar';
 import './index.scss';
 
 const Layout = () =>{
-    return (
-        <>
-            <LeftSidebar/>
-        </>
-    )
+  return (
+    <div className="App">
+      <LeftSidebar/>
+      <div className='page'>
+          <span className='tags top-tags'>&lt;body&gt;</span>
+          <Outlet />
+          <span className='tags bottom-tags'>
+          &lt;body&gt;
+          <br />
+          <span className='bottom-tag-html'>&lt;/html&gt;</span>
+          </span>
+      </div>
+    </div>
+  )
 }
 
 export default Layout;
